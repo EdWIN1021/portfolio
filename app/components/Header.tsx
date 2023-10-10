fa5ef39dc8f70ead835e602b18bc61604a7827de
Edwin <>
2023-10-09 22:14:00 -0700

    feat: add download button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,14 @@
+"use client";
+
 import Link from "next/link";
 
 const Header = () => {
+  const downloadCV = () => {
+    window.open(
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-3b344.appspot.com/o/Yang%20Shi%20Resume.docx?alt=media&token=3c55a47b-a001-446c-8f3a-02d37257305a"
+    );
+  };
+
   return (
     <div className="flex items-center justify-center p-3 w-[80%] m-auto">
       <div className="mr-10">
@@ -12,28 +20,36 @@ const Header = () => {
       </div>
 
       <nav className="flex-1 flex gap-5 text-[#f0f2f2] text-sm">
-        <Link href={"/"}>
+        <Link className="hover:text-[#d9dbdb]" href={"/"}>
           <span>Home</span>
         </Link>
 
-        <Link href={"/"}>
+        <Link className="hover:text-[#d9dbdb]" href={"/"}>
           <span>Services</span>
         </Link>
 
-        <Link href={"/"}>
+        <Link className="hover:text-[#d9dbdb]" href={"/"}>
           <span>Experience</span>
         </Link>
 
-        <Link href={"/"}>
+        <Link className="hover:text-[#d9dbdb]" href={"/"}>
           <span>Skills</span>
+        </Link>
+
+        <Link className="hover:text-[#d9dbdb]" href={"/"}>
+          <span>Contact</span>
         </Link>
       </nav>
 
-      <button className="bg-white text-[#161513] px-5 py-1 font-medium text-base rounded">
-        Contact
+      <button
+        className="bg-white hover:bg-[#d9dbdb] text-[#161513] px-5 py-1 font-medium text-base rounded-full"
+        onClick={downloadCV}
+      >
+        Download CV
       </button>
     </div>
   );
 };
 
 export default Header;
+//https://firebasestorage.googleapis.com/v0/b/portfolio-3b344.appspot.com/o/Yang%20Shi%20Resume.docx?alt=media&token=3c55a47b-a001-446c-8f3a-02d37257305a

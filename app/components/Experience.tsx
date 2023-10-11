fa3795d568beb556d1737e1f13982da3bfa43dce
Edwin <>
2023-10-10 21:50:41 -0700

    fix: ui

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -6,7 +6,10 @@ import Container from "../ui/Container";
 const Experience = () => {
   return (
     <Container bgColor={"#161513"}>
-      <h2 className="text-[32px] font-bold" id="experience">
+      <h2
+        className="text-[32px] font-bold text-center md:text-left"
+        id="experience"
+      >
         <span className="gradient_text">Experience</span>
       </h2>
 

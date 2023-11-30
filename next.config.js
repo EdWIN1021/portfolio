aab2fc4dded473c0d90186f0f94110c2454193ee
Edwin <>
2023-11-29 17:31:08 -0800

    fix: clean code

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   reactStrictMode: false,
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;

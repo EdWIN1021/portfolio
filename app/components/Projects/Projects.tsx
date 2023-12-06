f976a92fbe47922978c9e8b2d8877bdae0a152eb
Edwin <>
2023-12-06 07:49:51 -0800

    feat: update request

--- app/components/Projects/Projects.tsx
+++ app/components/Projects/Projects.tsx
@@ -1,45 +1,54 @@
+"use client";
+
 import Container from "@/app/ui/Container";
 import Heading from "@/app/ui/Heading";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProjectCard from "./ProjectCard";
 
-const projects = [
-  {
-    id: 1,
-    title: "Netflix Clone",
-    link: "https://temp-blue-seven.vercel.app/",
-    imgUrl: "/netflix-preview.png",
-  },
-  {
-    id: 2,
-    title: "Library Management System",
-    link: "https://library-management-system-teal.vercel.app/",
-    imgUrl: "/lms-preview.png",
-  },
-  {
-    id: 3,
-    title: "Twitter Clone",
-    link: "https://twitter-x-clone-kohl.vercel.app/",
-    imgUrl: "/twitter-preview.png",
-  },
-  {
-    id: 4,
-    title: "Airbnb Clone",
-    link: "https://air-clone-edwin1021.vercel.app/",
-    imgUrl: "/airbnb-preview.png",
-  },
-];
+interface Project {
+  id: number;
+  title: string;
+  link: string;
+  imgUrl: string;
+}
 
 const Projects = () => {
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(
+          "https://portfolio-3b344-default-rtdb.firebaseio.com/projects.json"
+        );
+        const data = await res.json();
+        setProjects(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
   return (
     <Container bgColor="#1c1c22">
       <Heading title="Projects"></Heading>
 
-      <div className="mt-[50px]">
-        {projects.map((project) => (
-          <ProjectCard key={project.id} project={project} />
-        ))}
-      </div>
+      {!loading && (
+        <div className="mt-[50px]">
+          {projects.map((project: Project) => (
+            <ProjectCard key={project?.id} project={project} />
+          ))}
+        </div>
+      )}
     </Container>
   );
 };

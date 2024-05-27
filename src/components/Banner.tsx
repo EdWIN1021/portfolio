6cd9de61b80c938e3664d97f92a756ef6bb74600
Edwin Shi <edwinshi1021@hotmail.com>
2024-05-27 14:29:41 -0700

    update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Container from "./Container";
 import IconButton from "./IconButton";
+import { motion } from "framer-motion";
 
 const Banner = () => {
   const [title, setTitle] = useState("");
@@ -31,39 +32,57 @@ const Banner = () => {
 
   return (
     <Container bgColor="#161513">
-      <img
-        className="rounded-2xl mx-auto"
-        src="https://firebasestorage.googleapis.com/v0/b/portfolio-3b344.appspot.com/o/32047952-21BC-443C-94A5-393035EEE550_1_105_c.jpeg?alt=media&token=26d32c1d-cef9-4b34-9b97-bb7e4f11b637"
-        alt="avatar..."
-        width={150}
-        height={200}
-      />
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={{
+          visible: { opacity: 1, y: 0, transition: { duration: 1.0 } },
+          hidden: { opacity: 0, y: 50 },
+        }}
+      >
+        <img
+          className="rounded-2xl mx-auto w-[150px] h-[200px]"
+          src="https://firebasestorage.googleapis.com/v0/b/portfolio-3b344.appspot.com/o/32047952-21BC-443C-94A5-393035EEE550_1_105_c.jpeg?alt=media&token=26d32c1d-cef9-4b34-9b97-bb7e4f11b637"
+          alt="avatar..."
+        />
 
-      <div className="text-center">
-        <h1 className="text-[32px] font-bold mt-5">
-          <p className="gradient_text">Hello, I'm Yang Shi (Edwin),</p>
-          {!loading && <p className="text-[#f0f2f5]">{title}</p>}
-        </h1>
+        <div className="flex flex-col items-center">
+          <h1 className="text-[32px] font-bold mt-5">
+            <p className="gradient_text text-center">
+              Hello, I'm Yang Shi (Edwin),
+            </p>
+            {!loading && <p className="text-[#f0f2f5] text-center">{title}</p>}
+          </h1>
 
-        <p className="mt-5 text-[#f0f2f5] text-sm">
-          Dynamic and qualified {title}. Adept in Computer Science education,
-          project management and programming. Strong analytical skills,
-          troubleshooting and problem-solving skills. Exceptional team player
-          with superior communication skills.
-        </p>
+          <p className="mt-5 text-[#f0f2f5] text-sm max-w-[800px]">
+            &nbsp;&nbsp;Highly skilled Software Developer with a robust
+            background in Computer Science and extensive experience in full
+            stack web development. Proven ability to manage complex projects and
+            deliver high-quality software solutions. Exceptional problem-solving
+            and analytical skills with a strong focus on collaboration and
+            communication. Seeking to leverage technical expertise and
+            innovative approach in a challenging development role to drive
+            technological advancements and business growth.
+          </p>
 
-        <div className="flex gap-5 item-center mt-8 justify-center md:justify-start text-[#f0f2f5]">
-          <div className="flex item-center gap-3">
-            <img src="position.png" alt="position..." width={20} height={20} />
-            <span>Canada</span>
+          <div className="flex gap-5 item-center mt-8 justify-center md:justify-start text-[#f0f2f5]">
+            <div className="flex item-center gap-3">
+              <img
+                src="position.png"
+                alt="position..."
+                width={20}
+                height={20}
+              />
+              <span>Canada</span>
+            </div>
+
+            <IconButton
+              url={"https://www.linkedin.com/in/yang-shi-2781b015b/"}
+              title={"linkedin"}
+            />
           </div>
-
-          <IconButton
-            url={"https://www.linkedin.com/in/yang-shi-2781b015b/"}
-            title={"linkedin"}
-          />
         </div>
-      </div>
+      </motion.div>
     </Container>
   );
 };
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -2,16 +2,25 @@ import SkillCard from "./SkillCard";
 import Container from "../Container";
 import Heading from "../Heading";
 import { skills } from "./constants";
+import { motion } from "framer-motion";
 
 const Skills = () => {
   return (
     <Container bgColor="#1c1c22">
-      <Heading title={"Skills"} />
-      <div className="grid md:grid-cols-6  grid-cols-4 gap-y-8 mt-10">
-        {skills.map((skill) => (
-          <SkillCard key={skill} imgUrl={`${skill}.png`} title={skill} />
-        ))}
-      </div>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        viewport={{ once: true }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.0 }}
+        exit={{ opacity: 1 }}
+      >
+        <Heading title={"Skills"} />
+        <div className="grid md:grid-cols-6  grid-cols-4 gap-y-8 mt-10">
+          {skills.map((skill) => (
+            <SkillCard key={skill} imgUrl={`${skill}.png`} title={skill} />
+          ))}
+        </div>
+      </motion.div>
     </Container>
   );
 };

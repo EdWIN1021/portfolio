9bcfafbd48942ecd40f309a9b1d7c8b6876f8dc9
Edwin Shi <edwinshi1021@hotmail.com>
2024-05-27 14:52:52 -0700

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
 
   const downloadCV = () => {
     window.open(
-      "https://firebasestorage.googleapis.com/v0/b/portfolio-3b344.appspot.com/o/Yang%20Shi%20Resume.docx?alt=media&token=3c55a47b-a001-446c-8f3a-02d37257305a"
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-3b344.appspot.com/o/YangShi-Resume.docx?alt=media&token=7509fea1-6819-46a4-a8bb-e72354851a03"
     );
   };
 

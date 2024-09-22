2dd30468013f12ce63b15bbbc93a450adaeee745
Edwin Shi <edwinshi1021@hotmail.com>
2024-09-22 14:09:01 -0700

    update

--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -16,19 +16,19 @@ const webComponents: { title: string; href: string; description: string }[] = [
     title: "Library Management System",
     href: "/work-samples/library-management-system",
     description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
+      "Developed a comprehensive Library Management System to manage library operations efficiently. The system includes features for user authentication, book transactions, inventory management, and fee processing.",
   },
   {
     title: "Twitter Clone",
     href: "/work-samples/twitter-clone",
     description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
+      "This project is a full-stack web application that replicates the core functionalities of Twitter, including user authentication, posting tweets, following users, and real-time updates.",
   },
   {
     title: "Real-time Chat Application",
     href: "/work-samples/real-time-chat-application",
     description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
+      "My project focuses on developing a real-time chat application designed to enhance user interaction through instant messaging capabilities. This application aims to provide a seamless experience for users, whether for personal conversations or professional collaborations.",
   },
 ];
 
@@ -37,13 +37,13 @@ const gameComponents: { title: string; href: string; description: string }[] = [
     title: "Shadow Knight",
     href: "/work-samples/shadow-knight",
     description:
-      "For sighted users to preview content available behind a link.",
+      "Shadow Knight is a fast-paced 2D platform where players navigate through dark, atmospheric levels filled with enemies. The game follows the journey of a shadow warrior who fights against evil creatures.",
   },
   {
     title: "Legend of Wukong",
     href: "/work-samples/legend-of-wukong",
     description:
-      "For sighted users to preview content available behind a link.",
+      "This game is an action-packed RPG inspired by Journey to the West, a classical Chinese mythological tale. Players take on the role of a legendary warrior, embarking on a quest through mystical lands, battling demons and mythical creatures using fast-paced, skill-based combat mechanics.",
   },
 ];
 
